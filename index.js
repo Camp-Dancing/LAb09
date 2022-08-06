a42068c521b20016e9a6ededf997e27b87d7815d
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-08-06 17:08:35 -0400

    setup the starter code

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-"use strict";
+'use strict';
+
+require('dotenv').config();
+const port = process.env.PORT ?? 3000;
+
+const server = require('./src/server.js');
+// const { db } = require('./src/models/index.js');
+// const { seed } = require('./src/models/seed');
+server.start(port);
+// db.sync()
+//   .then(async () => {
+//     if (process.env.NODE_ENV === 'dev') {
+//       await db.sync({ force: true });
+//       await seed();
+//     }
+//     server.start(port);
+//   })
+//   .catch(console.error);
--- src/server.js
+++ src/server.js
@@ -1,1 +1,28 @@
-"use strict";
+'use strict';
+
+require('dotenv').config();
+
+// const errorHandler = require('./error-handlers/500.js');
+// const logger = require('./middleware/logger.js');
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+// app.use(errorHandler);
+
+ app.use('*', (req, res) => {
+  res.status(200).send("HelllllO");
+ });
+
+
+
+ module.exports = {
+  server: app,
+  start: (port) => {
+    if (!port) {
+      throw new Error('Missing Port');
+    }
+    app.listen(port, () => console.log(`Listening on ${port}`));
+  },
+};
