6c1be9dfa57d78014a1f1c936baa96c72b3ff38b
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-08-06 17:50:07 -0400

    added user model and auth

---
+++ src/middleware/auth/route.js
@@ -0,0 +1,53 @@
+const { Users } = require('../../Models/index');
+
+// const express = require('express');
+const bcrypt = require('bcrypt');
+
+// const router = express.Router();
+
+
+async function signupUser(req, res) {
+
+  try {
+    let obj = req.body;
+    const doesNameExists = await Users.model.findOne({
+      where: { username: req.body.username },
+    });
+    if ( doesNameExists === null){
+      let newUsers = await Users.create(obj);
+      res.status(200).json(newUsers);
+
+    }else {
+      res.status(500).send(`cannot create user ${req.body.username}`);
+    }
+  }catch (e) {
+    console.log(e);
+  }
+}
+
+
+async function signinUser (req, res) {
+  try {
+    const user = await Users.model.findOne ({
+      where: { username: req.body.username },
+
+    });
+
+    const isValid = await bcrypt.compare(req.body.password, user.password);
+    if (isValid){
+      res.status(200).send({ user: user.username, token: user.token });
+      return;
+    }
+  }catch (e) {
+    res.status(500);
+  }
+  res.status(403).send('Invalid username/pasword. Too bad we don\'t have an account recovery mechanism');
+// }
+// router.post('/signup', signupUser);
+// router.post('/signin', signinUser);
+}
+
+module.exports = {
+  signinUser,
+  signupUser,
+};
--- src/server.js
+++ src/server.js
@@ -6,18 +6,22 @@ require('dotenv').config();
 // const logger = require('./middleware/logger.js');
 
 const express = require('express');
+const { signinUser, signupUser } = require('./middleware/auth/route');
 const app = express();
 
 app.use(express.json());
 // app.use(errorHandler);
+app.post('/signin', signinUser);
+app.put('/signup', signupUser);
 
- app.use('*', (req, res) => {
-  res.status(200).send("HelllllO");
- });
+app.use('*', (req, res) => {
+  res.status(200).send('HelllllO');
+});
 
 
 
- module.exports = {
+
+module.exports = {
   server: app,
   start: (port) => {
     if (!port) {
