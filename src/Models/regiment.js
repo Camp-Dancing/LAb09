4f42cd904f5291732c245e2eb3254b4880176ffe
Von <107226923+ArzuVon@users.noreply.github.com>
2022-08-06 17:44:59 -0400

    regiment model update with ID

--- src/Models/regiment.js
+++ src/Models/regiment.js
@@ -1,34 +1,13 @@
 'use strict';
 
 const regiment = (sequelize, DataTypes) => sequelize.define('Regiment', {
-  Monday: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  Tuesday: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  Wednesday: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  Thursday: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  Friday: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  Saturday: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  Sunday: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+    DayId: {
+        type: DataTypes.STRING,
+        references: {
+          model: 'exercise',
+          key: 'id'
+        }
+      },
 });
 
 module.exports = regiment;
