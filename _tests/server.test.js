8d6a1c9bae8a0c9390d863de4165918ede9f41fa
laptopadventure <bazelart13@gmail.com>
2022-08-06 15:00:09 -0700

    it compiles

--- _tests/server.test.js
+++ _tests/server.test.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
-const { server } = require("../src/server");
-const supertest = require("supertest");
+const { server } = require('../src/server');
+const supertest = require('supertest');
 const request = supertest(server);
-const logger = require("../src/middleware/logger");
+const logger = require('../src/middleware/logger');
 
-describe("server", () => {
-  describe("middleware", () => {
-    describe("logger", () => {
-      it("can log", async () => {
-        const spy = jest.spyOn(console, "log").mockImplementation();
-        await request.get("/");
+describe('server', () => {
+  describe('middleware', () => {
+    describe('logger', () => {
+      it('can log', async () => {
+        const spy = jest.spyOn(console, 'log').mockImplementation();
+        await request.get('/');
         expect(spy).toHaveBeenCalled();
       });
-      it("calls next", () => {
+      it('calls next', () => {
         const next = jest.fn();
         logger({}, {}, next);
         expect(next).toHaveBeenCalled();
--- src/middleware/auth/route.js
+++ src/middleware/auth/route.js
@@ -1,50 +1,49 @@
-const { Users } = require('../../Models/index');
+const { Users } = require('../../Models/users');
 
 // const express = require('express');
 const bcrypt = require('bcrypt');
 
 // const router = express.Router();
 
-
 async function signupUser(req, res) {
-
   try {
     let obj = req.body;
     const doesNameExists = await Users.model.findOne({
       where: { username: req.body.username },
     });
-    if ( doesNameExists === null){
+    if (doesNameExists === null) {
       let newUsers = await Users.create(obj);
       res.status(200).json(newUsers);
-
-    }else {
+    } else {
       res.status(500).send(`cannot create user ${req.body.username}`);
     }
-  }catch (e) {
+  } catch (e) {
     console.log(e);
   }
 }
 
-
-async function signinUser (req, res) {
+async function signinUser(req, res) {
   try {
-    const user = await Users.model.findOne ({
+    const user = await Users.model.findOne({
       where: { username: req.body.username },
-
     });
 
     const isValid = await bcrypt.compare(req.body.password, user.password);
-    if (isValid){
+    if (isValid) {
       res.status(200).send({ user: user.username, token: user.token });
       return;
     }
-  }catch (e) {
+  } catch (e) {
     res.status(500);
   }
-  res.status(403).send('Invalid username/pasword. Too bad we don\'t have an account recovery mechanism');
-// }
-// router.post('/signup', signupUser);
-// router.post('/signin', signinUser);
+  res
+    .status(403)
+    .send(
+      "Invalid username/pasword. Too bad we don't have an account recovery mechanism"
+    );
+  // }
+  // router.post('/signup', signupUser);
+  // router.post('/signin', signinUser);
 }
 
 module.exports = {
